fcaebc26d51bdcf1650cdddc5781ac20756b89f3
Kelsey Erickson <kerickson@gmx.com>
2021-08-17 21:12:27 +0000

    Adding move commands and connecting input to server

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function () {
+let connection;
+
+
+const setupInput = function (conn) {
+  connection = conn;
+ 
 
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -18,6 +23,23 @@ const handleUserInput = (key) => {
     process.exit();
   }
 
+  if(key === 'w'){
+    connection.write('Move: up')
+  }
+
+  if(key === 's'){
+    connection.write('Move: down')
+  }
+
+  if(key === 'a'){
+    connection.write('Move: left')
+  }
+
+  if(key === 'd'){
+    connection.write('Move: right')
+  }
+
 }
+
 
 module.exports.setupInput = setupInput;
